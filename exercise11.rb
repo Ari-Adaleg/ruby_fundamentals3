230650248fe4d2650fb91f7fb66c757dbbf6c055
Ari Drookman <aga.drookman@gmail.com>
2019-12-17 20:38:45 -0500

    Final version

--- exercise11.rb
+++ exercise11.rb
@@ -21,6 +21,7 @@ def call_absent_dogs(dog)
 		return "Come back, #{value}!"
 	end
 end
+
 my_dogs.each do |m|
 	puts call_absent_dogs(m)
 end
@@ -45,3 +46,48 @@ her_dogs = [
 her_dogs.each do |m|
 	puts call_absent_dogs(m)
 end
+
+####################
+# EXERCISE 5
+####################
+
+my_dogs = [
+ 	{
+ 		:name => 'Ralph',
+ 		:position => 5
+ 	},
+
+ 	{
+ 		:name => 'Cindy',
+ 		:position => 8
+ 	},
+
+ 	{
+ 		:name => 'Jade',
+ 		:position => 11
+ 	}
+]
+
+def call_absent_dogs(dog)
+	value = dog[:name]
+	if dog[:position] >= 10
+		return "Come back, #{value}!"
+	end
+end
+
+my_dogs.select { |m| puts call_absent_dogs(m)}
+
+def chase_squirrl(dog)
+	dog.map! do |x|
+		x[:position] + 5
+	end
+	return dog
+end
+
+puts chase_squirrl(my_dogs)
+
+return_dogs = my_dogs.map! do |y|
+	y -= y
+end
+
+puts return_dogs
